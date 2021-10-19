801b66e672b52f6bb7f09cad2e3b8dba36029b20
Thor Arne Løvberg <thorarne94@gmail.com>
2021-10-19 16:01:00 +0200

    no message

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -7,78 +7,133 @@ import 'package:get/get.dart';
 
 import 'home_controller.dart';
 
-// class Home extends StatefulWidget {
-//   const Home({Key? key}) : super(key: key);
-
-//   @override
-//   _HomeState createState() => _HomeState();
-// }
-
-// class _HomeState extends State<Home> {
-//   var quotes = [];
-//   @override
-//   Widget build(BuildContext context) {
-//     print(quotes);
-//     return Scaffold(
-//       body: Center(
-//         child: ListView(
-//           children: quotes.map((quote) {
-//             return ListTile(
-//               title: Text("hei på deg"),
-//             );
-//           }).toList(),
-//         ),
-//       ),
-//     );
-//   }
-
-//   getDio() async {
-//     try {
-//       var response =
-//           await Dio(BaseOptions(headers: {"Content-Type": "application/json"}))
-//               .get('https://type.fit/api/quotes');
-//       var data = response.data;
-//       setState(() {
-//         var quotes = json.decode(data);
-//         print(quotes[0]["text"]);
-//       });
-//     } catch (e) {
-//       print(e);
-//     }
-//   }
-// }
-
 class Home extends GetView<HomeController> {
   @override
-  Home() {
-    getDio();
-  }
-
-  getDio() async {
-    try {
-      var response =
-          await Dio(BaseOptions(headers: {"Content-Type": "application/json"}))
-              .get('https://type.fit/api/quotes');
-      var data = response.data;
-      controller.quotes.value = json.decode(data);
-      print(controller.quotes[0]["text"]);
-    } catch (e) {
-      print(e);
-    }
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ListView(
-          children: controller.quotes.map((quote) {
-            return Card(
-              child: Text(quote["text"]),
-            );
-          }).toList(),
-        ),
-      ),
-    );
+    return Obx(() => Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [
+              Colors.blue.shade50,
+              Colors.blue.shade200,
+              Colors.blue.shade400,
+              Colors.blue.shade600,
+            ],
+          )),
+          child: Scaffold(
+            backgroundColor: Colors.transparent,
+            body: Column(
+              children: [
+                Container(
+                    margin: EdgeInsets.only(top: 40, bottom: 40),
+                    child: Text(
+                      "Angst-meter",
+                      style: TextStyle(fontSize: 24),
+                    )),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: 25,
+                      child: Text(controller.sliderValueText),
+                    ),
+                    Column(
+                      children: [
+                        const Icon(
+                          Icons.thumb_down_sharp,
+                          color: Colors.white,
+                        ),
+                        RotatedBox(
+                          quarterTurns: 3,
+                          child: SizedBox(
+                            child: SliderTheme(
+                              data: SliderTheme.of(context).copyWith(
+                                  activeTrackColor: Colors.red.shade800,
+                                  inactiveTrackColor: Colors.green,
+                                  thumbColor: controller.getColor(),
+                                  trackHeight: 20,
+                                  thumbShape: RoundSliderThumbShape(
+                                    enabledThumbRadius: 20,
+                                  ),
+                                  overlayShape: RoundSliderOverlayShape(
+                                      overlayRadius: 25.0)),
+                              child: Slider(
+                                  min: 0,
+                                  max: 100,
+                                  value: controller.sliderValue.value,
+                                  onChanged: controller.onChanged),
+                            ),
+                          ),
+                        ),
+                        const Icon(
+                          Icons.thumb_up_alt_sharp,
+                          color: Colors.white,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 20),
+                  child: controller.getText(),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 20),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Column(
+                            children: [
+                              Text("Bredde"),
+                              Slider(
+                                  min: 0,
+                                  max: 200,
+                                  value: controller.sliderValueTwo.value,
+                                  onChanged: controller.onChangedTwo),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              Text("Høyde"),
+                              Slider(
+                                  min: 0,
+                                  max: 200,
+                                  value: controller.sliderValueThree.value,
+                                  onChanged: controller.onChangedThree),
+                            ],
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Text("Bredde: " +
+                                  controller.sliderValueTwoText +
+                                  "px"),
+                              Text("Høyde: " +
+                                  controller.sliderValueThreeText +
+                                  "px"),
+                            ],
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(top: 20),
+                            color: Colors.red,
+                            width: controller.sliderValueTwo.value,
+                            height: controller.sliderValueThree.value,
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ));
   }
 }
--- lib/screens/home/home_controller.dart
+++ lib/screens/home/home_controller.dart
@@ -1,5 +1,129 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class HomeController extends GetxController {
-  var quotes = [].obs;
+  RxDouble sliderValue = 0.0.obs;
+  RxDouble sliderValueTwo = 0.0.obs;
+  RxDouble sliderValueThree = 0.0.obs;
+
+  get sliderValueText => sliderValue.value.toInt().toString();
+  get sliderValueTwoText => sliderValueTwo.value.toInt().toString();
+  get sliderValueThreeText => sliderValueThree.value.toInt().toString();
+
+  void onChanged(double value) {
+    sliderValue.value = value.roundToDouble();
+    print(sliderValue.value);
+  }
+
+  void onChangedTwo(double value) {
+    sliderValueTwo.value = value.roundToDouble();
+    print(sliderValueTwo.value);
+  }
+
+  void onChangedThree(double value) {
+    sliderValueThree.value = value.roundToDouble();
+    print(sliderValueThree.value);
+  }
+
+  Color getColor() {
+    return Color.lerp(Colors.green, Colors.red, sliderValue.value / 100)!;
+  }
+
+  // Color getColor() {
+  //   if (sliderValue.value == 100) {
+  //     return Colors.red.shade900;
+  //   }
+  //   if (sliderValue.value > 75) {
+  //     return Colors.red.shade700;
+  //   }
+
+  //   if (sliderValue.value == 50) {
+  //     return Colors.white;
+  //   }
+  //   if (sliderValue.value > 25) {
+  //     return Colors.green.shade700;
+  //   }
+  //   if (sliderValue.value >= 0) {
+  //     return Colors.green.shade900;
+  //   }
+  //   return Colors.white;
+  // }
+
+  getText() {
+    if (sliderValue.value == 100) {
+      return Container(
+        padding: EdgeInsets.all(5),
+        alignment: Alignment.center,
+        height: 100,
+        width: 250,
+        color: getColor(),
+        child: Text(
+          "Nå er angsten din veldig høy, dette ser ut som er noe du må trene mer på og husk: øvelse \ngjør mester!",
+          style: TextStyle(fontSize: 16),
+        ),
+      );
+    }
+    if (sliderValue.value >= 75) {
+      return Container(
+        padding: EdgeInsets.all(5),
+        alignment: Alignment.center,
+        height: 50,
+        width: 200,
+        color: getColor(),
+        child: Text(
+          "Nå er det mer enn 75",
+          style: TextStyle(fontSize: 16),
+        ),
+      );
+    }
+    if (sliderValue.value > 50) {
+      return Container(
+        padding: EdgeInsets.all(5),
+        alignment: Alignment.center,
+        height: 50,
+        width: 200,
+        color: getColor(),
+        child: Text(
+          "Nå er det mer enn 50",
+          style: TextStyle(fontSize: 16),
+        ),
+      );
+    }
+    if (sliderValue.value == 50) {
+      return Container(
+        padding: EdgeInsets.all(5),
+        alignment: Alignment.center,
+        height: 50,
+        width: 200,
+        color: getColor(),
+        child: Text(
+          "Nå er den på midten",
+          style: TextStyle(fontSize: 16),
+        ),
+      );
+    }
+    if (sliderValue.value > 25) {
+      return Container(
+        padding: EdgeInsets.all(5),
+        alignment: Alignment.center,
+        height: 50,
+        width: 200,
+        color: getColor(),
+        child: Text(
+          "Nå er det mer enn 25",
+          style: TextStyle(fontSize: 16),
+        ),
+      );
+    }
+    return Container(
+        padding: EdgeInsets.all(5),
+        alignment: Alignment.center,
+        height: 50,
+        width: 200,
+        color: getColor(),
+        child: Text(
+          "Dette ser det ut som du takler bra, stå på!",
+          style: TextStyle(fontSize: 16),
+        ));
+  }
 }
